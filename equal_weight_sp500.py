7855e026e855bf16ed38c85733fda666dd99c05b
haiduk <hwlee91@gmail.com>
2021-01-03 22:15:08 -0500

    added api token

--- equal_weight_sp500.py
+++ equal_weight_sp500.py
@@ -2,7 +2,14 @@ import numpy as np
 import pandas as pd 
 # for making http requests
 import requests
-import XlsxWriter
+import xlsxwriter
 import math
 
-print (x)
+from secrets import IEX_CLOUD_API_TOKEN
+
+stocks = pd.read_csv('sp_500_stocks.csv')
+
+symbol = 'AAPL'
+api_url = f"https://sandbox.iexapis.com/stock/{symbol}/quote/?token={IEX_CLOUD_API_TOKEN}"
+
+print (api_url)
