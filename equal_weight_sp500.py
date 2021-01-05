3100a5654ddd534b1eef8a4eceaeeaea56d13d16
haiduk <hwlee91@gmail.com>
2021-01-05 08:10:20 -0500

    basic dataframe implemented

--- equal_weight_sp500.py
+++ equal_weight_sp500.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 # for making http requests
 import requests
+# for excel things
 import xlsxwriter
 import math
 
@@ -9,7 +10,23 @@ from secrets import IEX_CLOUD_API_TOKEN
 
 stocks = pd.read_csv('sp_500_stocks.csv')
 
-symbol = 'AAPL'
-api_url = f"https://sandbox.iexapis.com/stock/{symbol}/quote/?token={IEX_CLOUD_API_TOKEN}"
+my_columns = [ 'Ticker', 'Stock Price', 'Market Capitalization', 'Number of Shares to Buy' ]
+final_dataframe = pd.DataFrame(columns = my_columns)
 
-print (api_url)
+for stock in stocks['Ticker'][:5]:
+  api_url = f"https://sandbox.iexapis.com/stable/stock/{stock}/quote/?token={IEX_CLOUD_API_TOKEN}"
+  data = requests.get(api_url).json()
+  final_dataframe = final_dataframe.append(
+    pd.Series(
+      [
+        stock,
+        data['latestPrice'],
+        data['marketCap'],
+        'N/A'
+      ],
+      index = my_columns,
+    ),
+    ignore_index=True,
+  )
+
+print (final_dataframe)
