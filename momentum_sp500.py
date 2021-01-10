1468030b1ddfadebfe729e21dfa47cbedfe1e267
haiduk <hwlee91@gmail.com>
2021-01-10 03:59:46 -0500

    batch api call

--- momentum_sp500.py
+++ momentum_sp500.py
@@ -8,3 +8,38 @@ import xlsxwriter
 from secrets import IEX_CLOUD_API_TOKEN
 
 stocks = pd.read_csv('sp_500_stocks.csv')
+symbol = 'AAPL'
+
+api_url = f"https://sandbox.iexapis.com/stable/stock/{symbol}/stats?token={IEX_CLOUD_API_TOKEN}"
+data = requests.get(api_url).json()
+
+def chunks(lst, n):
+  for i in range(0, len(lst), n):
+    yield lst[i:i + n]
+
+symbol_groups = list(chunks(stocks['Ticker'], 100))
+symbol_strings = []
+for i in range(0, len(symbol_groups)):
+  symbol_strings.append(','.join(symbol_groups[i]))
+
+my_columns = ['Ticker', 'Price', 'One-Year Price Return', 'Number of Shares to Buy']
+
+final_dataframe = pd.DataFrame(columns = my_columns)
+
+for symbol_string in symbol_strings:
+  batch_api_call_url = F"https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=price,stats&token={IEX_CLOUD_API_TOKEN}"
+  data = requests.get(batch_api_call_url).json()
+  for symbol in symbol_string.split(','):
+    final_dataframe = final_dataframe.append(
+      pd.Series(
+        [
+          symbol, 
+          data[symbol]['price'],
+          data[symbol]['stats']['year1ChangePercent'],
+          'N/A'
+        ],
+        index = my_columns),
+        ignore_index = True
+      )
+
+print (final_dataframe)
