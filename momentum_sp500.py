2213831c4913d23b429f56a8809582d05f9ee02d
haiduk <hwlee91@gmail.com>
2021-01-11 15:44:08 -0500

    remove low-momentum stocks

--- momentum_sp500.py
+++ momentum_sp500.py
@@ -22,10 +22,13 @@ symbol_strings = []
 for i in range(0, len(symbol_groups)):
   symbol_strings.append(','.join(symbol_groups[i]))
 
+# make columns
 my_columns = ['Ticker', 'Price', 'One-Year Price Return', 'Number of Shares to Buy']
 
+# create empty dataframe with premade columns
 final_dataframe = pd.DataFrame(columns = my_columns)
 
+# call batch api to fill empty dataframe
 for symbol_string in symbol_strings:
   batch_api_call_url = F"https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=price,stats&token={IEX_CLOUD_API_TOKEN}"
   data = requests.get(batch_api_call_url).json()
@@ -42,4 +45,9 @@ for symbol_string in symbol_strings:
         ignore_index = True
       )
 
-print (final_dataframe)
+# remove low-momentum stocks
+# inplace modifies original dataframe
+final_dataframe.sort_values('One-Year Price Return', ascending = False, inplace = True)
+# resets index numbers to go from 0-49
+final_dataframe.reset_index(inplace = True)
+print(final_dataframe[:50])
