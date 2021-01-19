cd46cb3dfd5b77c5fc0f768f3785dd06d9616a1f
haiduk <hwlee91@gmail.com>
2021-01-19 17:19:08 -0500

    made 2nd value file for troubleshooting

--- 2value.py
+++ 2value.py
@@ -8,14 +8,8 @@ import math
 from secrets import IEX_CLOUD_API_TOKEN
 
 stocks = pd.read_csv('sp_500_stocks.csv')
-symbol = 'aapl'
-api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
-data = requests.get(api_url).json()
+portfolio_size = 100000
 
-price = data['latestPrice']
-pe_ratio = data['peRatio']
-
-# chunks takes a list + variable n and returns several lists, max 100 i.e. chunks a big list
 def chunks(lst, n):
   for i in range(0, len(lst), n):
     yield lst[i:i + n]
@@ -24,73 +18,6 @@ symbol_groups = list(chunks(stocks['Ticker'], 100))
 symbol_strings = []
 for i in range(0, len(symbol_groups)):
   symbol_strings.append(','.join(symbol_groups[i]))
-
-my_columns = ['Ticker', 'Price', 'P/E Ratio', 'Shares to Buy']
-
-final_dataframe = pd.DataFrame(columns = my_columns)
-for symbol_string in symbol_strings:
-  batch_api_call = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote&token={IEX_CLOUD_API_TOKEN}'
-  data = requests.get(batch_api_call).json()
-  for symbol in symbol_string.split(','):
-    final_dataframe = final_dataframe.append(
-      pd.Series(
-        [
-          symbol,
-          data[symbol]['quote']['latestPrice'],
-          data[symbol]['quote']['peRatio'],
-          0.0,
-        ],
-        index = my_columns,
-      ),
-      ignore_index = True
-    )
-
-# remove glamour stocks
-# sort, remove negative, trim, reset index, drop new index column
-final_dataframe.sort_values('P/E Ratio', inplace = True)
-final_dataframe = final_dataframe[final_dataframe['P/E Ratio'] > 0]
-final_dataframe = final_dataframe[:50]
-final_dataframe.reset_index(inplace = True)
-final_dataframe.drop('index', axis=1, inplace = True)
-
-def portfolio_input():
-  global portfolio_size
-  portfolio_size = input('Enter portfolio size:')
-
-  try: 
-    val = float(portfolio_size)
-  except ValueError:
-    print("That's not a number. \nTry again:")
-    portfolio_size = input('Enter portfolio size:')
-
-portfolio_size = 100000
-
-position_size = float(portfolio_size)/len(final_dataframe.index)
-for row in final_dataframe.index:
-  final_dataframe.loc[row, 'Shares to Buy'] = math.floor(position_size/final_dataframe.loc[row, 'Price'])
-
-# making a composite of valuation metrics for more realistic valuation
-symbol = 'AAPL'
-batch_api_call = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols=AAPL&types=quote,advanced-stats&token={IEX_CLOUD_API_TOKEN}'
-data = requests.get(batch_api_call).json()
-
-#P/E RATIO
-pe_ratio = data[symbol]['quote']['peRatio']
-
-# Price to book ratio
-pb_ratio = data[symbol]['advanced-stats']['priceToBook']
-
-# price to sale ratio
-ps_ratio = data[symbol]['advanced-stats']['priceToSales']
-
-# enterprise value divided by earnings before interest, taxes, depreciation, and amortization
-enterprise_value = data[symbol]['advanced-stats']['enterpriseValue']
-ebitda = data[symbol]['advanced-stats']['EBITDA']
-ev_to_ebitda = enterprise_value/ebitda
-
-# enterprice value divided by gross profit
-gross_profit = data[symbol]['advanced-stats']['grossProfit']
-ev_to_gp = enterprise_value/gross_profit
 
 # making robust value
 rv_columns = [
@@ -254,4 +181,4 @@ column_formats = {
 for column in column_formats.keys():
   writer.sheets['Value Strategy'].set_column(f'{column}:{column}', 25, column_formats[column][1])
   writer.sheets['Value Strategy'].write(f'{column}1', column_formats[column][0], column_formats[column][1])
-writer.save()
+#writer.save()
